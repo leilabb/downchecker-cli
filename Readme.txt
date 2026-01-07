f1004a458518b9bddff598a719152ba54ecf8612
Leila <leila.benhalima2@gmail.com>
2026-01-07 22:12:52 +0100

    add dns check

---
+++ Readme.txt
@@ -0,0 +1,7 @@
+3 checks:
+
+DNS resolution 
+
+TCP/connectivity check – try connecting to port 80/443.
+
+HTTP fetch & status code.
--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,7 +1,44 @@
 #!/usr/bin/env node
 
-console.log("yoooo");
+import nodeDns = require("node:dns");
 
-//console.log(process.argv.slice(2));
-//input: hello there
-//output [ 'hello', 'there' ]
+const readline = require("readline");
+const dns = require("node:dns").promises;
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+async function dnsCheck(url: string) {
+  try {
+    const records: any[] = await dns.resolveAny(url);
+
+    if (records.length > 0) {
+      console.log("DNS OK!");
+    }
+  } catch (error) {
+    console.log("DNS Failed");
+  }
+}
+
+async function start() {
+  try {
+    //Wrapping rl.question into a Promise to be able to await the user input
+    const url: string = await new Promise((resolve) =>
+      rl.question(`What's the name of the website you want to check? `, resolve)
+    );
+    rl.close();
+    dnsCheck(url);
+  } catch (error) {
+    console.log("Input error", error);
+  }
+}
+
+//Next:
+//Port check, httpfetch
+//All processes should be done simultaneously.
+//After each check, console log result.
+//Extra: Colored outputs
+
+start();
